84a2d69810228a7ebb03077708c36eeb77e63823
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-09-22 20:21:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,6 +3,6 @@ def square_array(array)
   counter = 0 
   new_array = []
   while counter < array.length() do
-  new_array.push(array[counter]**2)
+  new_array.push(array[counter] ** 2)
   counter++
 end
